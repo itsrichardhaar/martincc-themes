791b8c1fe7dd7f3a2cf860da0b883af4682aa4e7
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2026-03-03 14:16:08 -0500

    Add require templates.php to functions.php

--- martincc-parent/functions.php
+++ martincc-parent/functions.php
@@ -1,8 +1,7 @@
 <?php
 if ( ! defined('ABSPATH') ) exit;
 
-define('MARTINCC_THEME_VERSION', '1.0.0');
-
 require_once __DIR__ . '/inc/theme-setup.php';
 require_once __DIR__ . '/inc/enqueue.php';
 require_once __DIR__ . '/inc/acf.php';
+require_once __DIR__ . '/inc/templates.php';
